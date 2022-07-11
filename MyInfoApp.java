2e47850864803a4dc26d13110a1720db3b58487a
Bugsec <39850132+BuGhunTer-rd4@users.noreply.github.com>
2022-07-12 00:07:59 +0900

    Add files via upload

---
+++ MyInfoApp.java
@@ -0,0 +1,8 @@
+public class MyInfoApp {
+
+    public static void main(String [] args){
+        System.out.println("남동현");
+        System.out.println("24세");
+        System.out.println("중앙대학교 산업보안학과");
+    }
+}
